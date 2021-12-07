278ea4f8bf5215629f09121885074b1930081166
Taís Ribeiro <93293169+TaisRibeiroAReis@users.noreply.github.com>
2021-12-07 17:00:48 -0300

    Add files via upload

---
+++ LojaDeGames/src/main/java/lojaDeGames/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package lojaDeGames.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import lojaDeGames.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List <Produto> findAllByNomeContainingIgnoreCase(String Nome);
+
+}
---
+++ LojaDeGames/src/test/java/br/org/generation/LojaDeGames/LojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.LojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
